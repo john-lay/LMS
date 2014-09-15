7ca07166a7287e77f12285dfbadf0eb2a00333a0
john-lay <john_lay@hotmail.co.uk>
2014-09-15 16:50:17 +0100

    Initial check-in of WEB API and MVC app with LESS and Bootstrap.

---
+++ LMS.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using BundleTransformer.Core.Bundles;
+using BundleTransformer.Core.Orderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace LMS.Web
+{
+    public static class BundleConfig
+    {
+        public const string BootstrapPath = "~/Bundles/Bootstrap";
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery.validate.js",
+                "~/scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"));
+
+            var commonStylesBundle = new CustomStyleBundle(BootstrapPath);
+            commonStylesBundle.Orderer = new NullOrderer();
+
+            commonStylesBundle.Include(
+                "~/Content/bootstrap/bootstrap.less");
+
+            bundles.Add(commonStylesBundle);
+
+        }
+    }
+}
