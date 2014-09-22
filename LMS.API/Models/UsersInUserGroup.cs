280370422d258d57824ca58b6b3e68de24bb923b
john-lay <john_lay@hotmail.co.uk>
2014-09-22 09:40:19 +0100

    missing users in user group table

---
+++ LMS.API/Models/UsersInUserGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LMS.API.Models
+{
+    [Table("UsersInUserGroup")]
+    public class UsersInUserGroup
+    {
+        [Key, Column(Order = 0)]
+        [ForeignKey("UserGroup")]
+        public int UserGroupId { get; set; }
+
+        public virtual UserGroup UserGroup { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
