7a343aece563aa937e21df75e8cc07f2566b2178
john-lay <john_lay@hotmail.co.uk>
2014-12-29 11:05:53 +0000

    Added angular

---
+++ LMS.Web/Scripts/Client/ClientController.js
@@ -0,0 +1,15 @@
+﻿var ClientModule;
+(function (ClientModule) {
+    'use strict';
+
+    var ClientModuleController = (function () {
+        function ClientModuleController() {
+        }
+        return ClientModuleController;
+    })();
+    ClientModule.ClientModuleController = ClientModuleController;
+})(ClientModule || (ClientModule = {}));
+
+// Attach the controller to the app
+app.controller("ClientModuleController", ClientModule.ClientModuleController);
+//# sourceMappingURL=ClientController.js.map
---
+++ LMS.Web/Scripts/LMSapp.js
@@ -0,0 +1,1 @@
+﻿//# sourceMappingURL=LMSapp.js.map
